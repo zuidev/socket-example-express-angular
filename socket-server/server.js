7646026b2d0ef1998b4490cd69db9e06f89ef0a8
zuidev <baduy9780@gmail.com>
2023-02-18 23:55:01 +0900

    change file name

--- socket-server/server.js
+++ socket-server/server.js
@@ -1,6 +1,4 @@
 const app = require('express')();
-const cors = require('cors');
-
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
@@ -42,16 +40,6 @@ io.on('connection', (socket) => {
   io.emit('documents', Object.keys(documents));
 
   console.log(`Socket ${socket.id} connected`);
-});
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.get('/', (req, res) => {
-  console.log("hello");
 });
 
 http.listen(3000, () => {
